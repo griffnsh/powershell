e12fd9d079e054215f51afc0f58aa5c8057fa432
griffnsh <griffnshmail@gmail.com>
2024-08-23 09:50:05 -0500

    refactored PoSh modules script so that the variable can still be used further without format-table breaking it

--- get-installed-modules.ps1
+++ get-installed-modules.ps1
@@ -1,6 +1,4 @@
-# Get-Module -ListAvailable | Format-Table Name, Version, ModuleBase -AutoSize
 Write-Host "⏳ Gathering installed modules now details now..."
 
-$modules = Get-Module -ListAvailable | Format-Table Name, Version, ModuleBase -AutoSize
-
-$modules | Out-Host
+$modules = Get-Module -ListAvailable | Select-Object Name, Version, ModuleBase
+$modules | Format-Table -AutoSize
